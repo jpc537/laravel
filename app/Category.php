ee581581b8ce053d1569eacc9fd05d7cba384972
Esteban <jpc537@inlumine.ual.es>
2016-05-17 18:58:06 +0200

    Creacion de las relaciones entre tablas

--- app/Category.php
+++ app/Category.php
@@ -10,4 +10,11 @@ class Category extends Model
     protected $table = "categories";
 
     protected  $fillable = ['name'];
+
+    public function articles()
+    {
+        return $this->hasMany('App\Article');
+    }
+
+
 }
