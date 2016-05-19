601c5f51a01fae5d5e984832e99eab52db840cef
Esteban <jpc537@inlumine.ual.es>
2016-05-19 20:36:06 +0200

    Plantillas con blade

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -1,21 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{{$article->title}}</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/general.css')}}">
+</head>
+<body>
 
+    <h1>{{ $article->title}}</h1>
+    <hr>
+    {{$article->content}}
 
-
-
-
-<h1>{{ $article->title}}</h1>
-<hr>
-{{$article->content}}
-
-<hr>
-{{$article->user->name}} | {{$article->category->name}}
-@foreach($article->tags as $tag)
-    {{$tag->name}}
+    <hr>
+    {{$article->user->name}} | {{$article->category->name}}
+    @foreach($article->tags as $tag)
+        {{$tag->name}}
     @endforeach
 
-@for($i = 0; $i<=5; $i++)
-    {{$i}}
-@endfor
+    @for($i = 0; $i<=5; $i++)
+        {{$i}}
+    @endfor
+</body>
+</html>
+
+
+
 
 
 
