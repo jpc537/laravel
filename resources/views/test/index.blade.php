66c4ded409fb19bd5f7f8eec681b53177d6b49ef
Esteban <jpc537@inlumine.ual.es>
2016-05-19 19:14:37 +0200

    Vistas de laravel

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,20 @@
+
+
+
+
+
+<h1>{{ $article->title}}</h1>
+<hr>
+{{$article->content}}
+<hr>
+{{$article->user->name}} | {{$article->category->name}}
+@foreach($article->tags as $tag)
+    {{$tag->name}}
+    @endforeach
+
+@for($i = 0; $i<=5; $i++)
+    {{$i}}
+@endfor
+
+
+
