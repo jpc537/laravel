ae12c93a5ebff5b516f6ea06bd06ff6fbb3b042b
Esteban <jpc537@inlumine.ual.es>
2016-05-28 12:25:41 +0200

    Modificacion de la vista admin

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,34 @@
+<style>
+
+    div.transbox {
+        margin: 30px;
+        background-color: #ffffff;
+        opacity: 0.68;
+        filter: alpha(opacity=60); /* For IE8 and earlier */
+    }
+</style>
+@extends('app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="transbox">
+                <div class="col-md-3 pull-left">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li class="active"><a href="#">Inicio</a></li>
+                        <li><a href="{{route('admin.users.index') }}">Usuarios</a></li>
+                        <li><a href="{{route('admin.pistas.index') }}">Pistas</a></li>
+                        <li><a href="#">Reservas</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-md-8 col-md-offset">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Home</div>
+                    <div class="panel-body">Eres Administrador</div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
