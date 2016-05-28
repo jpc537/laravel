0a07ddae9715ad259695f1fed84a35455dfcea26
Esteban <jpc537@inlumine.ual.es>
2016-05-28 12:36:40 +0200

    He creado una nueva rama FEATURE

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -29,7 +29,7 @@
 					<span class="icon-bar"></span>
 
 				</button>
-				<a class="navbar-brand" href="#"><b>EnForma Sport center</b></a>
+				<a class="navbar-brand" href="#"><b>Viva Reservas</b></a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
