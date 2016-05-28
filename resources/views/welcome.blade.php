0c05cc0a45c409b7142297abc580684b735de435
Esteban <jpc537@inlumine.ual.es>
2016-05-28 19:20:13 +0200

    Continuamos con el welcome de laravel y seguimos cambiando fondo

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('app')
+@section('content')
+    <html>
+    <head>
+        <title>Viva Reservas</title>
+    </head>
+    <body backgroud class="vegas-background" src="../futbol.jpg">
+    <div class="container">
+        <div class="content">
+            <center> <h1>
+                    <div class="title">Viva Reservas</div></h1>
+            </center>
+        </div>
+    </div>
+    </body>
+    </html>
+@endsection
