c6f4936c52129e08de37e67ba71eb6dea93e7471
Esteban <jpc537@inlumine.ual.es>
2016-06-20 11:48:25 +0200

    config/database

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'hmis2015'),
+			'database'  => env('DB_DATABASE', 'bdhmis'),
 			'username'  => env('DB_USERNAME', 'root'),
 			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
