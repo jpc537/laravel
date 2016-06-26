fcc37379949f167b1945ac8719d9b562305e369d
tarikk <tarikimlahi@outlook.es>
2016-06-26 22:11:27 +0200

    probando el build

---
+++ tests/User2Test.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Tarik
+ * Date: 26/06/2016
+ * Time: 1:42
+ */
+class User2 extends PHPUnit_Framework_TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this-> assertTrue(true);
+    }
+
+
+
+}
